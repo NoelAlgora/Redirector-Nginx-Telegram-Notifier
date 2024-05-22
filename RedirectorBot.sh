2756588e2a7d4e06fb5bd60092a2837aaa150ef8
Noel Algora <noel.algora@gmail.com>
2024-05-22 22:50:56 +0200

    Update and rename GoogleBot.sh to RedirectorBot.sh

--- RedirectorBot.sh
+++ RedirectorBot.sh
@@ -7,7 +7,11 @@
 
 apiKey= PASTE YOUR TELEGRAM API KEY HERE
 chatID= PASTE YOUR TELEGRAM CHAT ID HERE
-message="GoogleBot HIT" # You can set your custom message here
+
+headerIdentifier="cd9452d3-9540-492d-b57b-7be28c4d2d29"
+
+message="Redirector HIT" # You can set your custom message here
+
 logFile=/var/log/nginx/access.log # This is default nginx log path. You can replace the path according to your setup
 
 tail -fn0 $logFile | \
@@ -15,10 +19,12 @@ while read line ; do
         echo "$line" | grep "Googlebot"
         if [ $? = 0 ]
         then
+          incoming_ip=`echo "$line" | awk '{printf $1}'`
+          redirector_host=`echo "$line" | awk '{printf $3}'`
           curl --silent --output /dev/null \
           -X POST \
            "https://api.telegram.org/bot$apiKey/sendMessage" \
-          -d text="$message" \
+          -d text="Redirector $redirector_host hit from ip $incoming_ip" \
           -d chat_id=$chatID
         fi
 done
